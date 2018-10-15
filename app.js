b2aa8d23b2ddd1c2c2a846957beb95303dcbb191
Anna Guk <guk.anna1989@gmail.com>
2018-10-16 01:20:40 +0200

    slider

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const h = document.getElementById("h");
 const m = document.getElementById("m");
+const slides = document.querySelectorAll("#sliderView .slide");
+const navs = document.querySelectorAll("#sliderNav .nav-bttn");
+
+const sliderView = document.querySelector("#sliderView");
 
 h.addEventListener("click", function() {
   if (m.style.display === "block") {
@@ -18,3 +22,42 @@ h.addEventListener("click", function() {
 //     const x = document.getElementById("myLinks");
 
 //   }
+
+const viewPortWidth = sliderView.offsetWidth;
+const viewPortHeight = sliderView.offsetHeight;
+sliderView.style.height = viewPortHeight + "px";
+
+let currentSlide = 0;
+
+slides.forEach((element, index) => {
+  element.style.display = "block";
+  element.style.position = "absolute";
+  element.style.left = index * viewPortWidth + "px";
+});
+
+navs.forEach(element => {
+  element.addEventListener("click", function(e) {
+    slide(this.innerText);
+  });
+});
+function slide(direction) {
+  if (direction === "Previous" && currentSlide === 0) {
+    return false;
+  } else {
+    if (direction === "Next" && currentSlide === slides.length - 1) {
+      return false;
+    } else {
+      let diff = 1;
+      if (direction === "Next") {
+        diff = -1;
+      } else {
+        diff = 1;
+      }
+      currentSlide -= diff;
+      slides.forEach(element => {
+        element.style.left =
+          parseInt(element.style.left, 10) + viewPortWidth * diff + "px";
+      });
+    }
+  }
+}
